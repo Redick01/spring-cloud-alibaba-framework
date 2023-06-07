81ee6464efb1f8901762be5d95d862af23283aa4
Redick01 <11090829@qq.com>
2023-06-07 20:38:00 +0800

    gray first

---
+++ ruuby-gateway/src/main/java/io/redick/cloud/account/filter/GrayscalePublishFilter.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.redick.cloud.account.filter;
+
+import io.redick.cloud.account.gray.GrayRequestContextHolder;
+import io.redick.cloud.gary.common.GrayConstant;
+import lombok.var;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+/**
+ * 灰度发布全局拦截器
+ *
+ * @author: Redick01
+ * @date: 2023/6/6 17:17
+ */
+@Component
+public class GrayscalePublishFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        var headers = exchange.getRequest().getHeaders();
+        if (headers.containsKey(GrayConstant.HEADER_GRAY_TAG)) {
+            Object garyValue = headers.get(GrayConstant.HEADER_GRAY_TAG);
+            if (!Objects.isNull(garyValue)
+                    && "true".equals(garyValue.toString())) {
+                GrayRequestContextHolder.setGrayTag(true);
+            }
+        }
+        var newRequest = exchange.getRequest().mutate()
+                .header(GrayConstant.HEADER_GRAY_TAG, GrayRequestContextHolder.getGrayTag().toString())
+                .build();
+        var newExchange = exchange.mutate()
+                .request(newRequest)
+                .build();
+        return chain.filter(newExchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ ruuby-gateway/src/main/java/io/redick/cloud/account/gray/GrayRequestContextHolder.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.redick.cloud.account.gray;
+
+
+/**
+ * @author: Redick01
+ * @date: 2023/6/7 20:01
+ */
+public class GrayRequestContextHolder {
+
+    private static final ThreadLocal<Boolean> GARY_TAG = new ThreadLocal<>();
+
+    public static void setGrayTag(final Boolean tag) {
+        GARY_TAG.set(tag);
+    }
+
+    public static Boolean getGrayTag() {
+        return GARY_TAG.get();
+    }
+
+    public static void remove() {
+        GARY_TAG.remove();
+    }
+}
